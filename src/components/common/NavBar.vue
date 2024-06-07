edf4f5f781ad2bb5d31701541924582c98b1dcfc
dino9881 <whdghthdms44@gmail.com>
2024-06-07 22:28:02 +0900

    feat: [UI] 메인페이지 화면 개발
    - 하단 NavBar 화면
    - 메인페이지 그룹리스트
    - 메인페이지 그룹아이템
    
    issue: #2

---
+++ src/components/common/NavBar.vue
@@ -0,0 +1,36 @@
+<script setup></script>
+
+<template>
+  <nav class="fixed-bottom border-top bg-white">
+    <div class="container-fluid d-flex justify-content-around align-items-center">
+      <RouterLink to="/notification">
+        <div>
+          <img src="/src/assets/imgs/bell-svgrepo-com.svg" width="25" alt="Bell Icon" />
+        </div>
+      </RouterLink>
+      <RouterLink to="/">
+        <div class="nav-link text-center">
+          <img
+            class="rounded-pill"
+            src="/src/assets/imgs/default-img.webp"
+            alt="Logo"
+            width="70"
+            height="50"
+          />
+        </div>
+      </RouterLink>
+      <RouterLink to="/mypage">
+        <div>
+          <img src="/src/assets/imgs/person-svgrepo-com.svg" width="35" alt="Person Icon" />
+        </div>
+      </RouterLink>
+    </div>
+  </nav>
+</template>
+
+<style scoped>
+nav {
+  z-index: 100;
+  background-color: white;
+}
+</style>
