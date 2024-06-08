5b0147b576e6bd04684a7340c3e32c3ad2d1b9fc
dino9881 <whdghthdms44@gmail.com>
2024-06-08 13:16:20 +0900

    feat: [UI] 알림페이지 화면 개발
    - 알림 리스트
    - 알림 아이템
    - 알림- 투표 페이지
    
    issue: #4

---
+++ src/components/notification/VoteOpenModal.vue
@@ -0,0 +1,88 @@
+<script setup>
+import { defineProps } from 'vue'
+
+const props = defineProps({
+  comment: {
+    type: String,
+    required: true
+  },
+  closeModal: {
+    type: Function,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <div class="modal-overlay" @click.self="closeModal">
+    <div class="modal-dialog-centered">
+      <div class="modal-wrapper">
+        <div class="modal-content">
+          <div class="modal-header">
+            <div class="border-bottom border-2 mb-2 ms-2 w-100">
+              <h5 class="modal-title mb-1">{{ comment }}</h5>
+            </div>
+          </div>
+          <div class="modal-body mb-3 d-flex flex-column ms-2 info-msg">
+            <span>정말로</span>
+            <span>마일리지를 사용하여</span>
+            <span>친구를 열람하시겠어요?</span>
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-sm open-btn" @click="closeModal">확인</button>
+            <button type="button" class="btn btn-sm close-btn ms-2" @click="closeModal">
+              취소
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.modal-overlay {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.2);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  z-index: 1050;
+}
+
+.modal-dialog-centered {
+  max-width: 500px;
+  width: 90%;
+}
+.modal-wrapper {
+  background-color: white;
+  width: 80vw;
+  padding: 20px;
+  border-radius: 10px;
+}
+.close-btn,
+.open-btn {
+  border: #ff7a00 1px solid;
+  color: #ff7a00;
+  transition:
+    background-color 0.3s,
+    color 0.3s;
+}
+.close-btn:hover,
+.close-btn:focus,
+.close-btn:active,
+.open-btn:hover,
+.open-btn:focus,
+.open-btn:active {
+  background-color: #ff7a00;
+  color: white;
+}
+
+.info-msg {
+  font-size: 18px;
+}
+</style>
--- src/views/notification/Notification.vue
+++ src/views/notification/Notification.vue
@@ -1,7 +1,78 @@
-<script setup></script>
+<script setup>
+import { ref } from 'vue'
+import NavBar from '@/components/common/NavBar.vue'
+import NotificationItem from '@/components/notification/NotificationItem.vue'
+
+const notis = ref([
+  {
+    date: '2023.10.13',
+    title: '누군가 나를 선택했어요!',
+    description: '마성의 매력이 있는 친구',
+    type: 'vote',
+    count: 2
+  },
+  {
+    date: '2023.10.13',
+    title: '누군가 나를 선택했어요!',
+    description: '책을 많이 읽을 것 같은 사람',
+    type: 'vote',
+    count: 4
+  },
+  {
+    date: '2023.10.13',
+    title: '결과가 나왔어요!',
+    description: '제일 귀여운 친구',
+    type: 'vote-result'
+  },
+  {
+    date: '2023.10.12',
+    title: '새로운 그룹에 초대되었어요!',
+    description: '우리들만의 공간',
+    type: 'invitation'
+  }
+])
+</script>
 
 <template>
-  <h1>알림</h1>
+  <div class="container">
+    <div class="text-left mt-5 ms-4 main-title">
+      <p>My News</p>
+    </div>
+    <div class="text-center mt-5">
+      <div v-if="notis.length === 0" class="mt-5">
+        <h4>아직 새로운 소식이 없어요</h4>
+      </div>
+      <div v-for="(noti, index) in notis" :key="index">
+        <NotificationItem
+          :date="noti.date"
+          :title="noti.title"
+          :description="noti.description"
+          :type="noti.type"
+          :count="noti.count"
+        />
+      </div>
+    </div>
+    <NavBar></NavBar>
+  </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.container {
+  min-height: 90vh;
+}
+.main-title {
+  border-bottom: 1px solid #ff7a00;
+  font-size: 32px;
+  font-weight: 500;
+}
+.noti-btn:hover,
+.noti-btn:focus,
+.noti-btn:active {
+  background-color: #ff7a00;
+  color: white;
+}
+
+footer i {
+  background-color: black;
+}
+</style>
