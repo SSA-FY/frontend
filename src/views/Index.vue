7ec9a1a95342022dd2bdc1157d108562c518537e
Aiden-Jung <hyunseok0314@gmail.com>
2024-06-10 01:25:49 +0900

    feat: #9 [UI] add main page

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -1,7 +1,24 @@
-<script setup></script>
+<script setup>
+import LogoMark from '@/components/logos/LogoMark.vue'
+import LogoText from '@/components/logos/LogoText.vue'
+
+import BottomOrangeButton from '@/components/BottomOrangeButton.vue'
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+
+const redirectMain = () => {
+  router.push('/group')
+}
+</script>
 
 <template>
-  <h1>메인페이지</h1>
+  <div class="tw-w-full tw-h-screen tw-flex tw-flex-col tw-justify-center tw-items-center">
+    <div class="tw-flex tw-justify-center">
+      <LogoMark class="tw-w-[60px]" />
+      <LogoText class="tw-ms-2 tw-text-[2.5rem]" />
+    </div>
+    <div class="tw-mt-2 tw-font-medium">익명 칭찬은 람다에서</div>
+  </div>
+  <BottomOrangeButton @click="redirectMain">지금 바로 시작하기</BottomOrangeButton>
 </template>
-
-<style scoped></style>
