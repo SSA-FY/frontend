f3f67652b9fcbcf471c600eaa8d124549757735e
Aiden-Jung <hyunseok0314@gmail.com>
2024-06-10 01:24:44 +0900

    feat: #9 [UI] add logos

---
+++ src/components/logos/LogoText.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>Lambda</div>
+</template>
+
+<style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Comfortaa:wght@300..700&display=swap');
+
+div {
+  font-family: 'Comfortaa';
+  font-optical-sizing: auto;
+  font-weight: 700;
+  font-style: normal;
+}
+</style>
