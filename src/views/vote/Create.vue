68438431b31e3a24b87414a6ad01646295e8a7b4
왕승철 <ys4512558@naver.com>
2024-06-11 03:01:03 +0900

    feat: [API] 투표생성 페이지 API연동

--- src/views/vote/Create.vue
+++ src/views/vote/Create.vue
@@ -1,9 +1,56 @@
 <script setup>
 import { ref } from 'vue'
-import Suggestions from '@/components/vote/Suggestions.vue'
-import VoteButton from '@/components/vote/VoteButton.vue'
-
+import { useRouter } from 'vue-router'
+import Suggestions from '@/components/vote/create/Suggestions.vue'
+import VoteButton from '@/components/vote/create/VoteButton.vue'
+import voteAPI from '@/apis/vote.js'
 const selectedSuggestion = ref('')
+const question = ref('')
+const memberId = ref('')
+const teamId = ref('')
+
+const change = (payload) => {
+  selectedSuggestion.value = payload
+}
+
+const router = useRouter()
+
+const moveGroupPage = () => {
+  router.push('/group')
+}
+
+const createVote = () => {
+  // memberId.value = '5a574b34-a3af-4b3c-bf7a-82376d70b853'
+  memberId.value = 1
+  teamId.value = 1
+  if (question.value != '') {
+    voteAPI.createVote(
+      memberId.value,
+      teamId.value,
+      { content: question.value, backgroundUrl: 'null' },
+      () => {
+        moveGroupPage()
+      },
+      () => {
+        console.log('저장실패')
+      }
+    )
+  } else if (selectedSuggestion.value != null && selectedSuggestion.value != '') {
+    voteAPI.createVote(
+      memberId.value,
+      teamId.value,
+      { content: selectedSuggestion.value, backgroundUrl: 'null' },
+      () => {
+        moveGroupPage()
+      },
+      () => {
+        console.log('저장실패')
+      }
+    )
+  } else {
+    alert('질문을 입력해주세요')
+  }
+}
 </script>
 
 <template>
@@ -19,16 +66,18 @@ const selectedSuggestion = ref('')
           class="form-control border border-2 border-black rounded-0"
           rows="4"
           placeholder="질문을 입력하세요"
+          v-model="question"
         ></textarea>
       </div>
-      <Suggestions v-model="selectedSuggestion" />
-      <VoteButton></VoteButton>
+      <Suggestions v-model="selectedSuggestion" @selectSuggestions="change" />
     </div>
+    <VoteButton class="p-0" @click="createVote">투표 생성</VoteButton>
   </div>
 </template>
 
 <style scoped>
 .card {
+  border: 0px;
   margin-top: 20px;
 }
 </style>
