f8d717ae02f06dd095872b1810eab90d8a05b311
dino9881 <whdghthdms44@gmail.com>
2024-06-11 15:53:20 +0900

    chore: build bootstrap dependency 제거

--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 import './assets/reset.css'
 import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-icons/font/bootstrap-icons.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
