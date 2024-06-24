7dab500b0e61257fce478645e06b5adb3721fcdd
SeungCheol <ys4512558@naver.com>
2024-06-25 03:26:10 +0900

    feat: [UI] 투표 결과 (득표자 퍼센트 및 득표) 컴포넌트 개발

---
+++ src/components/board/VoteResultItem.vue
@@ -0,0 +1,39 @@
+<script setup>
+import { computed } from 'vue'
+import createBoardAPI from '@/apis/board.js'
+
+const boardAPI = createBoardAPI()
+
+const props = defineProps({
+  item: Object
+})
+
+console.log('item:', props.item)
+const gradientStyle = computed(
+  () => `background: linear-gradient(to right, #ff7a00, white ${props.item.percent}%)`
+)
+</script>
+
+<template>
+  <div class="d-flex align-items-center my-2 p-2 gradient rounded" :style="gradientStyle">
+    <img
+      :src="item.profileImgUrl"
+      class="rounded-circle"
+      style="width: 40px; height: 40px; margin-right: 10px"
+    />
+    <div class="flex-grow-1">
+      <p class="mb-0">{{ item.member }}</p>
+      <span class="text-muted">{{ item.username }}</span>
+    </div>
+    <div class="text-end">
+      <p class="mb-0 text-orange fw-bold fs-5">{{ item.percent }}%</p>
+      <span class="fw-bold">{{ item.voteCnt }}표</span>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.text-orange {
+  color: #ff7a00;
+}
+</style>
