303478e8d08359b5c970a751aad3905cfb3ea201
왕승철 <ys4512558@naver.com>
2024-06-26 18:15:35 +0900

    feat: [UI] TopBackWard, NavBar 추가 및 페이지 구조 변경

--- src/views/board/List.vue
+++ src/views/board/List.vue
@@ -2,6 +2,8 @@
 import { ref, watch } from 'vue'
 import { useRouter } from 'vue-router'
 import createBoardAPI from '@/apis/board'
+import NavBar from '@/components/common/NavBar.vue'
+import TopBackward from '@/components/vote/vote/TopBackward.vue'
 
 const boardAPI = createBoardAPI()
 const teamId = ref(1)
@@ -53,35 +55,43 @@ const detailPage = (boardId) => {
 }
 </script>
 <template>
+  <TopBackward></TopBackward>
+
   <div class="container">
-    <h1>지난 투표 리스트</h1>
-    <div v-for="boardList in dailyBoardList" :key="boardList.time">
+    <div class="text-left mt-5 ms-2 main-title">
+      <p>My Board</p>
+    </div>
+    <div v-for="boardList in dailyBoardList" :key="boardList.time" class="ms-4 mt-3 me-4">
       <div class="row">
         <div class="col-12">
-          <div class="my-2 text-decoration-underline fw-bold">
+          <div class="row my-2 text-decoration-underline fw-bold">
             {{ boardList.time }}
+            <hr class="my-3" style="border: 2px solid" />
           </div>
+
           <div>
             <div v-for="(board, index) in boardList.boardItems" :key="board.id" class="row my-1">
-              <div class="row">
-                <p class="fw-bold">{{ board.content }}</p>
-              </div>
-              <div class="row my-1">
-                <div class="col-9 d-flex align-items-center">
-                  <img
-                    v-for="(imgUrl, index) in board.memberList"
-                    :key="index"
-                    :src="imgUrl"
-                    class="rounded-circle mx-2"
-                    style="width: 40px; height: 40px"
-                  />
+              <div class="bg-orange-opacity pb-3 rounded pt-2">
+                <div class="row">
+                  <p class="fw-bold">{{ board.content }}</p>
                 </div>
-                <div
-                  class="col-3 d-flex align-items-center justify-content-end"
-                  @click="detailPage(board.boardId)"
-                >
-                  댓글 보기
-                  <!-- <i class="fa-solid fa-message fa-2x orange"></i> -->
+                <div class="row my-1">
+                  <div class="col-9 d-flex align-items-center">
+                    <img
+                      v-for="(imgUrl, index) in board.memberList"
+                      :key="index"
+                      :src="imgUrl"
+                      class="rounded-circle mx-2"
+                      style="width: 40px; height: 40px"
+                    />
+                  </div>
+                  <div
+                    class="col-3 d-flex align-items-center justify-content-end"
+                    @click="detailPage(board.boardId)"
+                  >
+                    댓글
+                    <!-- <i class="fa-solid fa-message fa-2x orange"></i> -->
+                  </div>
                 </div>
               </div>
               <hr class="my-3" :style="index == boardList.length - 1 ? 'border: 2px solid;' : ''" />
@@ -91,18 +101,31 @@ const detailPage = (boardId) => {
       </div>
     </div>
   </div>
+  <NavBar></NavBar>
 </template>
 
 <style scoped>
-.card-header {
-  background-color: #f8f9fa;
-  font-weight: bold;
+.container {
+  min-height: 90vh;
 }
 
 img {
   object-fit: cover;
 }
 .orange {
-  color: orange;
+  color: #ff7a00;
+}
+.bg-orange {
+  background-color: #ff7a00;
+}
+
+.bg-orange-opacity {
+  background-color: #ff7a00;
+}
+
+.main-title {
+  border-bottom: 1px solid #ff7a00;
+  font-size: 32px;
+  font-weight: 500;
 }
 </style>
