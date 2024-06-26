e2c0eff7ab6448eb464703deffd3ff2c99c0d351
q0uu0p <q0_u_0p@naver.com>
2024-06-26 13:20:35 +0900

    feat: [API] add axios to create group

---
+++ src/apis/invite.js
@@ -0,0 +1,18 @@
+import { lambdaAxios } from '@/utils/axios'
+
+const createInviteAPI = () => {
+  const inviteAxios = lambdaAxios()
+  return {
+    searchMember: (tag, success, fail) => {
+      inviteAxios.get(`/member/search?tag=${encodeURIComponent(tag)}`).then(success).catch(fail)
+    },
+    createInvites: (teamId, memberList, success, fail) => {
+      inviteAxios
+        .post(`/invitation?teamId=${teamId}`, memberList )
+        .then(success)
+        .catch(fail)
+    },
+  }
+}
+
+export default createInviteAPI
