0e5d5762913ba9b840bddd7a3548236f96dc1c08
왕승철 <ys4512558@naver.com>
2024-06-26 18:11:36 +0900

    feat: [UI, API] 투표 완료 컴포넌트 수정 - [UI] 마일리지 -> 포인트로 명명 수정 - [API] 오늘의 투표 정보를 받아오는 API 연동

--- src/components/vote/finish/Finish.vue
+++ src/components/vote/finish/Finish.vue
@@ -1,19 +1,36 @@
-<script setup></script>
+<script setup>
+import { ref } from 'vue'
+import createVoteAPI from '@/apis/vote'
+
+const voteAPI = createVoteAPI()
+const todayInfo = ref(Object)
+const getTodayVoteInfo = () => {
+  voteAPI.getTodayInfo(
+    ({ data }) => {
+      todayInfo.value = data
+    },
+    () => {
+      console.log('오늘의 투표 정보를 가져오는데 실패했습니다.')
+    }
+  )
+}
+getTodayVoteInfo()
+</script>
 
 <template>
-  <h1>투표 완료</h1>
+  <h2>오늘의 투표 🎉</h2>
   <div class="p-3 bg-light">
     <div class="border-bottom py-2 d-flex justify-content-between align-items-center">
-      <span>오늘 획득한 마일리지</span>
-      <span class="fw-bold"> <span class="emoji">😊</span> + 5000P </span>
+      <span>오늘 획득한 포인트</span>
+      <span class="fw-bold"> <span class="emoji">😊</span> + {{ todayInfo.totalPoint }}P </span>
     </div>
     <div class="border-bottom py-2 d-flex justify-content-between align-items-center">
       <span>오늘 투표 수</span>
-      <span class="fw-bold">6개</span>
+      <span class="fw-bold">{{ todayInfo.voteInfoCnt }}개</span>
     </div>
     <div class="py-2 d-flex justify-content-between align-items-center">
       <span>오늘 한 마디 수</span>
-      <span class="fw-bold">3개</span>
+      <span class="fw-bold">{{ todayInfo.opinionCnt }}개</span>
     </div>
   </div>
 </template>
