9f2031d527767d581cb8adf4c1ce605f35013edb
왕승철 <ys4512558@naver.com>
2024-06-26 18:09:23 +0900

    feat: [API, UI] 댓글 작성, 수정, 삭제 API 호출 코드 개발 및 UI 수정

--- src/components/board/Comment.vue
+++ src/components/board/Comment.vue
@@ -1,48 +1,84 @@
 <script setup>
 import { useMemberStore } from '@/stores/memberStore'
 import { storeToRefs } from 'pinia'
-import { computed } from 'vue'
-import createCommentAPI from '@/apis/comment';
+import { ref, computed, watch } from 'vue'
+import createCommentAPI from '@/apis/comment'
 
-const commentAPI = createCommentAPI();
+const commentAPI = createCommentAPI()
 const store = useMemberStore()
 const { tag } = storeToRefs(store)
 const props = defineProps({
   comment: Object
 })
-const content = computed(()=>props.comment.content)
-const content2HTML = computed(() => props.comment.content.replace('\n', '<br/>'))
-const myComment = computed(() => props.comment.commenter.split('@')[1] == tag.value)
+const emit = defineEmits(['reloadComment'])
 
+const comment = ref(props.comment)
+watch(
+  () => props.comment,
+  () => {
+    comment.value = props.comment
+    comment.value.content = comment.value.content.replaceAll('\\n', '\n')
+  },
+  { immediate: true }
+)
+const content2HTML = computed(() => comment.value.content.replaceAll('\n', '<br/>'))
+const myComment = computed(() => comment.value.commenter.split('@')[1] == tag.value)
+const editContent = ref(null)
 const isEditing = ref(false)
 
 const saveEditComment = () => {
-  editing.value = false
-  
+  console.log(editContent.value)
+  commentAPI.editComment(
+    props.comment.commentId,
+    editContent.value,
+    () => {
+      props.comment.content = editContent.value
+    },
+    () => {
+      console.log('수정 실패')
+    }
+  )
+  isEditing.value = false
 }
-
-const editing = ()=>isEditing.value = true
+const cancelEdit = () => (isEditing.value = false)
+const editing = () => {
+  isEditing.value = true
+  editContent.value = comment.value.content
+}
+const removeComment = () => {
+  commentAPI.deleteComment(
+    props.comment.commentId,
+    () => {
+      emit('reloadComment')
+    },
+    () => {
+      console.log('삭제 실패')
+    }
+  )
+}
 </script>
 
 <template>
   <div class="p-2 my-2" :class="[myComment ? 'my' : 'other']">
     <div v-if="!!myComment" class="d-flex justify-content-end">
-      <template v-if="isEditing">
-        <button  class="btn btn-success p-1 m-1" @click="editing()">수정</button>
+      <template v-if="!isEditing">
+        <button class="btn btn-success p-1 m-1" @click="editing()">수정</button>
         <button class="btn btn-danger p-1 m-1" @click="removeComment()">삭제</button>
       </template>
       <template v-else>
-        <button class="btn btn-success p-1 m-1" @click="saveEditComment()">저장</button>
-        <button class="btn btn-danger p-1 m-1" @click="removeComment()">취소</button>
+        <button class="btn btn-primary p-1 m-1" @click="saveEditComment()">저장</button>
+        <button class="btn btn-orange p-1 m-1" @click="cancelEdit()">취소</button>
       </template>
     </div>
     <div v-if="!myComment">
       <span class="fw-bold">{{ comment.commenter }}</span>
     </div>
-    <p>
-      <v-if></v-if>
-      <div v-html="content2HTML"></div>
-    </p>
+    <div>
+      <div v-if="!isEditing" v-html="content2HTML"></div>
+      <div v-else>
+        <textarea v-model="editContent" class="form-control" rows="3"></textarea>
+      </div>
+    </div>
     <div class="text-end text-muted">
       {{ comment.time }}
     </div>
@@ -62,5 +98,10 @@ const editing = ()=>isEditing.value = true
   border-top-right-radius: 10px;
   border-bottom-left-radius: 10px;
   border-bottom-right-radius: 10px;
+}
+
+.btn-orange {
+  background-color: #ff7a00;
+  color: white;
 }
 </style>
