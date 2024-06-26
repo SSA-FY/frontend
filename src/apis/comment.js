876c593632967d5db97d9ef4fc14a3a89cc96fa1
왕승철 <ys4512558@naver.com>
2024-06-26 18:07:38 +0900

    feat: [API] comment 관련 API 요청 형식 수정

--- src/apis/comment.js
+++ src/apis/comment.js
@@ -4,16 +4,16 @@ const createCommentAPI = () => {
   const commentAxios = lambdaAxios()
   return {
     writeComment: (boardId, content, success, fail) => {
-      commentAxios.post(`/comment/${boardId}`, { content }).then(success).catch(fail)
+      commentAxios.post(`/comment/${boardId}`, content).then(success).catch(fail)
     },
     getComments: (boardId, page, success, fail) => {
       commentAxios.get(`/comment/${boardId}`, { params: { page } }).then(success).catch(fail)
     },
     editComment: (commentId, content, success, fail) => {
-      commentAxios.put(`comment/${commentId}`, { content }).then(success).catch(fail)
+      commentAxios.put(`/comment/${commentId}`, content).then(success).catch(fail)
     },
     deleteComment: (commentId, success, fail) => {
-      commentAxios.delete(`comment/${commentId}`).then(success).catch(fail)
+      commentAxios.delete(`/comment/${commentId}`).then(success).catch(fail)
     }
   }
 }
