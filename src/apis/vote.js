45dbca680a984977b05914d15a303571f2b1e476
SeungCheol <ys4512558@naver.com>
2024-07-01 16:55:02 +0900

    feat: [API] teamId -> teamName으로 매개변수 수정

--- src/apis/vote.js
+++ src/apis/vote.js
@@ -3,9 +3,9 @@ import { lambdaAxios } from '@/utils/axios'
 const createVoteAPI = () => {
   const voteAxios = lambdaAxios()
   return {
-    createVote: (teamId, formData, success, fail) => {
+    createVote: (teamName, formData, success, fail) => {
       voteAxios
-        .post(`/vote?teamId=${teamId}`, formData, {
+        .post(`/vote?teamName=${teamName}`, formData, {
           headers: {
             'Content-Type': 'multipart/form-data'
           }
@@ -13,11 +13,11 @@ const createVoteAPI = () => {
         .then(success)
         .catch(fail)
     },
-    getVoteList: (teamId, success, fail) => {
-      voteAxios.get(`/vote/list`, { params: { teamId } }).then(success).catch(fail)
+    getVoteList: (teamName, success, fail) => {
+      voteAxios.get(`/vote/list`, { params: { teamName } }).then(success).catch(fail)
     },
-    getMemberItems: (teamId, success, fail) => {
-      voteAxios.get(`/membership/team/${teamId}`).then(success).catch(fail)
+    getMemberItems: (teamName, success, fail) => {
+      voteAxios.get(`/team/member`, { params: { teamName } }).then(success).catch(fail)
     },
     getMemberResultItems: (voteId, success, fail) => {
       voteAxios.get(`/vote/${voteId}`).then(success).catch(fail)
