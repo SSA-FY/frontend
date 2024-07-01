eb800f4e3b1b92ad5480e4b0ff185e659b50bc44
SeungCheol <ys4512558@naver.com>
2024-07-01 16:55:39 +0900

    feat: teamName을 사용하며 pathVariable대신 Query사용으로 변경

--- src/apis/board.js
+++ src/apis/board.js
@@ -3,8 +3,8 @@ import { lambdaAxios } from '@/utils/axios'
 const createBoardAPI = () => {
   const boardAxios = lambdaAxios()
   return {
-    getBoardList: (teamId, page, success, fail) => {
-      boardAxios.get(`/boards/${teamId}`, { params: { page } }).then(success).catch(fail)
+    getBoardList: (teamName, page, success, fail) => {
+      boardAxios.get(`/boards`, { params: { teamName, page } }).then(success).catch(fail)
     },
     getBoardDetail: (boardId, page, success, fail) => {
       boardAxios.get(`/board/${boardId}`, { params: { page } }).then(success).catch(fail)
